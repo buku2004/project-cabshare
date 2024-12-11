1236f6735f7c24cf1f012da7b57953c97928c826
Subhransu Dalei <subhransu2004@gmail.com>
2024-12-11 21:00:09 +0530

    few changes #5

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Content = () => {
   return (
-    <div className="bg-gray-800 text-white p-10 px-[15rem]">
+    <div className="bg-gray-800 text-white p-6 md:p-10 lg:px-[15rem]">
       {/* Smart Ride Sharing Title */}
-      <div className="mb-6">
+      <div className="mb-6 text-center md:text-left">
         <div className="text-white inline-block rounded-md font-semibold text-sm uppercase">
           Smart Ride Sharing
         </div>
@@ -15,18 +15,20 @@ const Content = () => {
       </div>
 
       {/* Feature Cards */}
-      <div className="grid md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Card 1 */}
         <div className="bg-gray-900 shadow-lg p-6 rounded-lg group hover:scale-105 transform transition-transform duration-300">
           <img
             src="./userauth.webp"
             alt="User Authentication"
-            className="w-full h-48 object-cover rounded-md"
+            className="w-full h-48 object-center rounded-md"
           />
-          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-110 transition-transform duration-300"><Link href="/user-auth" >
-            User Authentication
-          </Link></h3>
-          <p className="text-gray-300 mt-2 text-xl">
+          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-105 transition-transform duration-300">
+            <Link href="/user-auth">
+              User Authentication
+            </Link>
+          </h3>
+          <p className="text-gray-300 mt-2 text-sm md:text-xl">
             Secure login and profile setup for users. <br /> Only for NITR-Junta.
           </p>
         </div>
@@ -36,12 +38,14 @@ const Content = () => {
           <img
             src="./addlisting.webp"
             alt="Create Ride Listings"
-            className="w-full h-48 object-cover rounded-md"
+            className="w-full h-48 object-center rounded-md"
           />
-          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-110 transition-transform duration-300"><Link href="/add-listings" >
-            Create Ride Listings
-          </Link></h3>
-          <p className="text-gray-300 mt-2 text-xl">
+          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-105 transition-transform duration-300">
+            <Link href="/add-listings">
+              Create Ride Listings
+            </Link>
+          </h3>
+          <p className="text-gray-300 mt-2 text-sm md:text-xl">
             Enter details like destination, time, and date to create a listing,
             allowing others to connect for sharing the ride.
           </p>
@@ -52,12 +56,14 @@ const Content = () => {
           <img
             src="alllistings.webp"
             alt="Show All Listings"
-            className="w-full h-48 object-cover rounded-md"
+            className="w-full h-48 object-center rounded-md"
           />
-          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-110 transition-transform duration-300"><Link href="/all-listings">
-            Show All Listings
-            </Link></h3>
-          <p className="text-gray-300 mt-2 text-xl">
+          <h3 className="text-2xl font-semibold mt-4 group-hover:text-green-500 group-hover:scale-105 transition-transform duration-300">
+            <Link href="/all-listings">
+              Show All Listings
+            </Link>
+          </h3>
+          <p className="text-gray-300 mt-2 text-sm md:text-xl">
             Explore all the ride listings available for all days, organized by
             departure times and destinations. Easily connect with fellow
             students traveling on the same days.
