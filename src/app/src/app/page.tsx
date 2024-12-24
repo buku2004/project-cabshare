8d2532ad5dce534de2bd523e216f2c83aa577a6a
krishna singh <153911304+Kri01ceram@users.noreply.github.com>
2024-12-24 14:39:27 +0530

    Add files via upload

---
+++ src/app/src/app/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from './components/Footer' 
+import Content from './components/Content'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Feedback from './components/Feedback'
+
+const page = () => {
+  return (
+    <div> 
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Content/>
+      
+      <Feedback/>
+      <Footer/>
+    </div>
+  )
+}
+export default page
