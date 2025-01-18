09d4c4769523642682cc05f8d9ae37fcde7b9121
krishna singh <153911304+Kri01ceram@users.noreply.github.com>
2025-01-18 06:51:12 +0530

    Add files via upload

--- src/app/constants/firebase.js
+++ src/app/constants/firebase.js
@@ -1,24 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
-  authDomain: "cabshare-14491.firebaseapp.com",
-  projectId: "cabshare-14491",
-  storageBucket: "cabshare-14491.firebasestorage.app",
-  messagingSenderId: "156449845303",
-  appId: "1:156449845303:web:8cf8c15d2b2c5f59914fb2",
-  measurementId: "G-JDN9P03YSG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-export const app1 = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app1);
- export const db = getFirestore(app1);
-//  export default db;
+export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+// export default db;
 
-// auth 
-export const auth = getAuth(app1);
+// Auth
+export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
